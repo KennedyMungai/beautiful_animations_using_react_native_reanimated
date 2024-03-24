863a7912b33aaa70766b97b3e748b4c2e148011f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-24 17:13:59 +0300

    Added a custom back button to the details page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,31 @@
-import { Stack } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import { Stack, useNavigation } from 'expo-router';
 import React from 'react';
-
+import { TouchableOpacity } from 'react-native';
 import '../global.css';
 
 const MainPageLayout = () => {
+  const navigation = useNavigation();
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerTransparent: true, headerShown: false }} />
       <Stack.Screen
         name="cityDetails/[cityId]"
-        options={{ headerTransparent: true, headerTitle: '' }}
+        options={{
+          headerTransparent: true,
+          headerTitle: '',
+          headerLeft: () => (
+            <TouchableOpacity>
+              <Ionicons
+                name="chevron-back"
+                size={30}
+                className="bg-white/80 rounded-full p-2"
+                onPress={() => navigation.goBack()}
+              />
+            </TouchableOpacity>
+          ),
+        }}
       />
     </Stack>
   );

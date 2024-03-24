97bcd6991ebcc9e7833dd623bc18af91a37f67ce
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-24 16:52:49 +0300

    Created and registered the cityDetails page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,10 @@ const MainPageLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerTransparent: true, headerShown: false }} />
+      <Stack.Screen
+        name="cityDetails/[cityId]"
+        options={{ headerShown: false, headerTransparent: true }}
+      />
     </Stack>
   );
 };
---
+++ app/cityDetails/[cityId].tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CityDetails = () => {
+  return (
+    <View>
+      <Text>CityDetails</Text>
+    </View>
+  );
+};
+
+export default CityDetails;

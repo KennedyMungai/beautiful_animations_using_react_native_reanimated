ac0b26d137bd798ab5c5b2217921b55d3a24d28e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-24 17:00:54 +0300

    Showing the city details page

--- app/cityDetails/[cityId].tsx
+++ app/cityDetails/[cityId].tsx
@@ -1,11 +1,21 @@
+import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Image, SafeAreaView, Text, View } from 'react-native';
+
+import { Cities } from '~/assets/cities';
 
 const CityDetails = () => {
+  const { cityId } = useLocalSearchParams();
+
+  const cityDetails = Cities.find((city) => city.id === Number(cityId));
+
   return (
-    <View>
-      <Text>CityDetails</Text>
-    </View>
+    <SafeAreaView className="bg-white flex-1">
+      <Image source={{ uri: cityDetails?.image }} className="w-full h-[400]" />
+      <View>
+        <Text>CityDetails</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 

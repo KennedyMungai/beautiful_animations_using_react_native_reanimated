f72c52979aa20ec7bbaf68bc721b7ef7fc3d0742
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-24 16:49:12 +0300

    Rendered out the cities in a flatlist

--- app/index.tsx
+++ app/index.tsx
@@ -5,16 +5,26 @@ import { Cities } from '~/assets/cities';
 
 const MainAppPage = () => {
   return (
-    <SafeAreaView className="flex-1 bg-white items-center justify-center px-2 pt-12">
-      <View>
+    <SafeAreaView className="flex-1 bg-white px-2 pt-12">
+      <View className="flex items-center justify-center pb-8">
         <FlatList
           data={Cities}
           keyExtractor={(item) => item.id.toString()}
+          showsVerticalScrollIndicator={false}
+          ListHeaderComponent={() => (
+            <Text className="text-center text-2xl text-neutral-700 font-bold  ">
+              Some of our finest destinations
+            </Text>
+          )}
           numColumns={2}
           renderItem={({ item }) => (
-            <View>
-              <Image source={{ uri: item.image }} className="aspect-video" />
-              <Text>{item.name}</Text>
+            <View className="m-1">
+              <Image
+                source={{ uri: item.image }}
+                className="w-52 h-52 rounded-md"
+                resizeMode="cover"
+              />
+              <Text className="text-xl font-bold text-neutral-600">{item.name}</Text>
             </View>
           )}
         />

d5893d5a9b201086bb371f75bc6af12185901c8f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-24 16:08:55 +0300

    Created an array of african cities with their images

---
+++ assets/cities.ts
@@ -0,0 +1,15 @@
+export const cities = [
+  { name: 'Nairobi, Kenya', image: 'https://source.unsplash.com/300x200/?nairobi' },
+  { name: 'Cape Town, South Africa', image: 'https://source.unsplash.com/300x200/?cape+town' },
+  { name: 'Cairo, Egypt', image: 'https://source.unsplash.com/300x200/?cairo' },
+  { name: 'Lagos, Nigeria', image: 'https://source.unsplash.com/300x200/?lagos' },
+  {
+    name: 'Johannesburg, South Africa',
+    image: 'https://source.unsplash.com/300x200/?johannesburg',
+  },
+  { name: 'Khartoum, Sudan', image: 'https://source.unsplash.com/300x200/?khartoum' },
+  { name: 'Dar es Salaam, Tanzania', image: 'https://source.unsplash.com/300x200/?dar+es+salaam' },
+  { name: 'Accra, Ghana', image: 'https://source.unsplash.com/300x200/?accra' },
+  { name: 'Alexandria, Egypt', image: 'https://source.unsplash.com/300x200/?alexandria+egypt' },
+  { name: 'Casablanca, Morocco', image: 'https://source.unsplash.com/300x200/?casablanca' },
+];

a3d85e6a7d55b0cc344de6a0ac5f16a2b94d3b5f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-24 16:36:56 +0300

    Rendered out the items on the data file

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, FlatList, Image } from 'react-native';
+
+import { Cities } from '~/assets/cities';
 
 const MainAppPage = () => {
   return (
-    <SafeAreaView className="flex-1 bg-white items-center justify-center p-5">
+    <SafeAreaView className="flex-1 bg-white items-center justify-center px-2 pt-12">
       <View>
-        <Text>MainAppPage</Text>
+        <FlatList
+          data={Cities}
+          keyExtractor={(item) => item.id.toString()}
+          numColumns={2}
+          renderItem={({ item }) => (
+            <View>
+              <Image source={{ uri: item.image }} className="aspect-video" />
+              <Text>{item.name}</Text>
+            </View>
+          )}
+        />
       </View>
     </SafeAreaView>
   );
